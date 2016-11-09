3409337687056b2e8db570f5c994ea5940b8a451
pablotdvsm <pablotdvsm@gmail.com>
2016-11-09 20:41:17 -0200

    Estudos angular js

---
+++ AngularJSSinglePageApplication/src/AngularJSSinglePageApplication/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularJSSinglePageApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Teste()
+        {
+            return View();
+        }
+
+        public IActionResult Teste2()
+        {
+            return View();
+        }
+
+        public IActionResult Teste3()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ AngularJSSinglePageApplication/src/AngularJSSinglePageApplication/wwwroot/app/controllers.js
@@ -0,0 +1,7 @@
+﻿// moduler
+var personApp = angular.module('personApp', []);
+
+personApp.controller('personController', function ($scope) {
+    $scope.firstName = "Pablo";
+    $scope.lastName = "Tôndolo";
+});
