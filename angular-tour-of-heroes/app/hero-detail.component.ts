4110f1fce8930956d07d1dd8b8adc471da196b34
pablotdvsm <pablotdvsm@gmail.com>
2016-12-24 14:36:25 -0200

    Commit

--- angular-tour-of-heroes/app/hero-detail.component.ts
+++ angular-tour-of-heroes/app/hero-detail.component.ts
@@ -1,15 +1,15 @@
 import 'rxjs/add/operator/switchMap';
-import { Component, OnInit }      from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Location }               from '@angular/common';
+import { Location } from '@angular/common';
 
-import { Hero }         from './hero';
-import { HeroService }  from './hero.service';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 @Component({
   moduleId: module.id,
   selector: 'my-hero-detail',
   templateUrl: 'hero-detail.component.html',
-  styleUrls: [ 'hero-detail.component.css' ]
+  styleUrls: ['hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero;
@@ -18,7 +18,7 @@ export class HeroDetailComponent implements OnInit {
     private heroService: HeroService,
     private route: ActivatedRoute,
     private location: Location
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.route.params
@@ -29,6 +29,12 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
+
 }
 
 
