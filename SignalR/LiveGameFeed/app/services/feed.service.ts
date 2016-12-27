111149674e6109d8463c6ed477aa4ba0f66faf59
Pablo Tondolo 2 <pablotondolo@SYNS.CORP>
2016-12-27 15:02:24 -0200

    Continuar

--- SignalR/LiveGameFeed/app/services/feed.service.ts
+++ SignalR/LiveGameFeed/app/services/feed.service.ts
@@ -16,4 +16,5 @@ export class FeedService {
     setConnectionId: Observable<string>;
     updateMatch: Observable<Match>;
     //addFeed:
+    //https://chsakell.com/2016/10/10/real-time-applications-using-asp-net-core-signalr-angular/
 }
