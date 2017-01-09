8819946f018bb7cf651db0dc5ea04115efff7919
pablotdvsm <pablotdvsm@gmail.com>
2017-01-09 20:38:26 -0200

    Commit

--- CoreWeb/Program.cs
+++ CoreWeb/Program.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+using System;
 
-namespace MvcApp
+class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
-        }
+        Console.WriteLine("Hello World!");
     }
 }
