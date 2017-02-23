304c89546355fee2b98a6f994b848a094a2ca43c
Pablo Tondolo 2 <pablotondolo@SYNS.CORP>
2017-02-23 10:35:23 -0300

    Commit

---
+++ FirstAngular2/Startup.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+
+namespace FirstAngular2
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
