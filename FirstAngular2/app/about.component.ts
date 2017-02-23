a3222115bfe92143c71f9841a567a9417d0b0eeb
Pablo Tondolo 2 <pablotondolo@SYNS.CORP>
2017-02-23 14:00:22 -0300

    Commit

--- FirstAngular2/app/about.component.ts
+++ FirstAngular2/app/about.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     moduleId: module.id,
-    selector: 'my-about',    
-    template: `
-        <h2>About Sriracha</h2>
-        <p>This is what Wikipedia says about <a href="https://en.wikipedia.org/wiki/Sriracha_sauce_%28Huy_Fong_Foods%29">Sriracha sauce</a>:</p>
-        <blockquote>It can be recognized by its bright red color and its packaging: a clear plastic bottle with a green cap, text in Vietnamese, English, Chinese, French, and Spanish, and the rooster logo. David Tran was born in 1945, the Year of the Rooster...<blockquote>
-    `,
+    selector: 'my-app',
+    template: 'Hello, Angular 2 World',
 })
-export class AboutComponent { }
+export class AppComponet { }
--- FirstAngular2/app/app.module.ts
+++ FirstAngular2/app/app.module.ts
@@ -1,27 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { PairingComponent } from './pairing.component';
-import { AboutComponent } from './about.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: '/pairing', pathMatch: 'full' },
-  { path: 'pairing',  component: PairingComponent },
-  { path: 'about', component: AboutComponent },
-];
 
 @NgModule({
-    bootstrap: [ AppComponent ],
-    imports: [ 
-        BrowserModule,
-        RouterModule.forRoot(routes),
-    ],
-    declarations: [ 
-        AppComponent, 
-        PairingComponent, 
-        AboutComponent, 
-    ],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule],
+    declarations: [AppComponent],
 })
 export class AppModule { }
