a2ed6465379bdcae240306c498c7e0577c1e1571
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-02-27 14:47:12 -0300

    Xamarin - FormsGallery (ContentPage)

---
+++ Xamarin/FormsGallery/FormsGallery/FormsGallery/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+
+namespace FormsGallery
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {            
+            InitializeComponent();
+
+            NavigateCommand = new Command<Type>(async (Type pageType) =>
+            {
+                Page page = (Page)Activator.CreateInstance(pageType);
+                await Navigation.PushAsync(page);
+            });
+
+            BindingContext = this;
+        }
+
+        public ICommand NavigateCommand { private set; get; }
+    }
+ }
