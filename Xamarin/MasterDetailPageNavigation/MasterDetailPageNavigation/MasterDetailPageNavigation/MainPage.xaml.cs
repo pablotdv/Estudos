d36125f20daef080a4bf81571dd9f3da0af379a1
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-02-28 14:56:33 -0300

    Xamarin Samples (MasterDetailPageNavigation)

--- Xamarin/MasterDetailPageNavigation/MasterDetailPageNavigation/MasterDetailPageNavigation/MainPage.xaml.cs
+++ Xamarin/MasterDetailPageNavigation/MasterDetailPageNavigation/MasterDetailPageNavigation/MainPage.xaml.cs
@@ -12,6 +12,24 @@ namespace MasterDetailPageNavigation
         public MainPage()
         {
             InitializeComponent();
+
+            masterPage.ListView.ItemSelected += OnItemSelected;
+
+            if (Device.OS == TargetPlatform.Windows)
+            {
+                Master.Icon = "swap.png";
+            }
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                masterPage.ListView.SelectedItem = null;
+                IsPresented = false;
+            }
         }
     }
 }
