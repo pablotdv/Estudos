4712631c248af1da9311696a0344a970a84d3063
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-03-21 10:26:32 -0300

    Exemplo TDD

---
+++ TDD/MvcContacts/MvcContacts/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcContacts.Models
+{
+    [Table("Contacts")]
+    public class Contact
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required()]
+        public string FirstName { get; set; }
+        [Required()]
+        public string LastName { get; set; }
+        [RegularExpression(@"^\d{3}-?\d{3}-?\d{4}$")]
+        public string Phone { get; set; }
+        [Required()]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$")]
+        public string Email { get; set; }
+    }
+}
