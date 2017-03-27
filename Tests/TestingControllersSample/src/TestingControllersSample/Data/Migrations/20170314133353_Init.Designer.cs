016e569f60a088cf60f0ab4f561d44dc59c3f456
pablotdvsm <pablotdvsm@gmail.com>
2017-03-27 12:27:51 -0300

    school of net - Curso AspNet Core mvc EF

---
+++ Tests/TestingControllersSample/src/TestingControllersSample/Data/Migrations/20170314133353_Init.Designer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TestingControllersSample.Models;
+
+namespace TestingControllersSample.Data.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20170314133353_Init")]
+    partial class Init
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("TestingControllersSample.Models.StormSession", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTimeOffset>("DateCreated");
+
+                    b.Property<int>("IdeaCount");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("StormSessions");
+                });
+        }
+    }
+}
