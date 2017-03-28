44889d23a365ae734d72e792c4d3cf315cbbe69d
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-03-27 22:22:08 -0300

    Iniciando com C#

--- schoolofnet/CursoIniciandoComCSharp/csharp-fundamentos/Program.cs
+++ schoolofnet/CursoIniciandoComCSharp/csharp-fundamentos/Program.cs
@@ -6,8 +6,15 @@ namespace csharp_fundamentos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int matricula = 10;
+            string nome = "Pablo";
+            string sobrenome = "Tôndolo de Vargas";
+            DateTime dataNascimento = new DateTime(198, 12, 12);
+            decimal salario = 1000.77M;
+            bool feriasVencidas = true;
+            char sexo = 'M';
+
+            Console.WriteLine($"{nome} {sobrenome}");
         }
     }
 }
