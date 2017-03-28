0fa821b3fa69c8a66aaa0d4be9f112f9d05f9156
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-03-27 22:54:34 -0300

    Operadores aritiméticos

--- schoolofnet/CursoIniciandoComCSharp/csharp-fundamentos/Program.cs
+++ schoolofnet/CursoIniciandoComCSharp/csharp-fundamentos/Program.cs
@@ -6,15 +6,32 @@ namespace csharp_fundamentos
     {
         static void Main(string[] args)
         {
-            int matricula = 10;
-            string nome = "Pablo";
-            string sobrenome = "Tôndolo de Vargas";
-            DateTime dataNascimento = new DateTime(198, 12, 12);
-            decimal salario = 1000.77M;
-            bool feriasVencidas = true;
-            char sexo = 'M';
+            // int matricula = 10;
+            // string nome = "Pablo";
+            // string sobrenome = "Tôndolo de Vargas";
+            // DateTime dataNascimento = new DateTime(198, 12, 12);
+            // decimal salario = 1000.77M;
+            // bool feriasVencidas = true;
+            // char sexo = 'M';
 
-            Console.WriteLine($"{nome} {sobrenome}");
+            // Console.WriteLine($"{nome} {sobrenome}");
+
+            int a = 15;
+            int b = 2;
+            int soma = a + b;
+            Console.WriteLine(soma);
+
+            int diferenca = a - b;
+            Console.WriteLine(diferenca);
+
+            int multiplicacao = a * b;
+            Console.WriteLine(multiplicacao);
+
+            decimal divisao = (decimal)a / (decimal)b;
+            Console.WriteLine(divisao);
+
+            int divisao2  = a % b;
+            Console.WriteLine(divisao2);
         }
     }
 }
