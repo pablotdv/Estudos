bd2ab6d0a0a8948129026bbf335c9d673d522bbf
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-03-27 22:14:06 -0300

    Iniciando com C#

---
+++ schoolofnet/CursoIniciandoComCSharp/csharp-fundamentos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace csharp_fundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
