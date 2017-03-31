b21edebc009431bc2be0eebd5304c2d444b297bb
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-03-30 22:05:33 -0300

    ASP.NET MVC com TDD - Primeiros Passos

---
+++ schoolofnet/MVC_TDD/MVC_TDD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_TDD.Startup))]
+namespace MVC_TDD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
