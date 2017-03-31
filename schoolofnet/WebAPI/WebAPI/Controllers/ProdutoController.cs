5f9d030b3f4f2e92957b2ab5b3f54cda8cdff230
pablotdvsm <pablotdvsm@gmail.com>
2017-03-31 17:51:01 -0300

    Curso Web API

---
+++ schoolofnet/WebAPI/WebAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ProdutoController : ApiController
+    {
+        Produto[] produtos = new Produto[]
+        {
+            new Produto() { Codigo = 1, Nome = "Nokia Lumia", Categoria = "Celulares", Valor = 699.99M },
+            new Produto() { Codigo = 2, Nome = "Samsung Galaxy", Categoria = "Celulares", Valor = 1199.99M },
+            new Produto() { Codigo = 3, Nome = "Teclado", Categoria = "Informática", Valor = 130.00M },
+            new Produto() { Codigo = 4, Nome = "Mouse", Categoria = "Informática", Valor = 59.95M },
+            new Produto() { Codigo = 5, Nome = "Case GoPro", Categoria = "Acessórios", Valor = 65.00M },
+        };
+
+        
+        public IEnumerable<Produto> GetTodos()
+        {
+            return produtos;
+        }
+        
+        public IHttpActionResult Get(int codigo)
+        {
+            var produto = produtos.FirstOrDefault(a => a.Codigo == codigo);
+
+            if (produto == null)
+                return NotFound();
+
+            return Ok(produto);
+        }
+
+        public IEnumerable<Produto> GetPorCategoria(string categoria)
+        {
+            var produtos = this.produtos.Where(a => a.Categoria == categoria);
+
+            
+            return produtos;
+        }
+
+
+    }
+}
---
+++ schoolofnet/WebAPI/WebAPI/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public String Nome { get; set; }
+        public string Categoria { get; set; }
+        public decimal Valor { get; set; }
+
+    }
+}
