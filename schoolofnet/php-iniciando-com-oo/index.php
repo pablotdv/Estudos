34b31cc34a0543c527d5551b7aafe4d94934d5c0
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-04-03 22:32:45 -0300

    Curso - PHP - Iniciando com OO

--- schoolofnet/php-iniciando-com-oo/index.php
+++ schoolofnet/php-iniciando-com-oo/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "<h1>Starting with OO<h1>";
+$input = array(22,34,17,18,14,13,22,25);
+$filtro = function(int $age) {
+    return ($age >= 18);
+};
+
+$output = array_filter($input, function(int $age) {
+    return ($age >= 18);
+});
+
+print_r($output);
