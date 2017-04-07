5a24cc20ca132010f7d64816e6df5cb0e6665617
Pablo Tôndolo de Vargas <pablotondolo@syns.com.br>
2017-04-07 15:28:47 -0300

    PHP - Avançando com OO

---
+++ schoolofnet/php-avancando-com-oo/code_04/Conn.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("IConn.php");
+
+class Conn implements IConn
+{
+    private $dsn;
+    private $user;
+    private $pass;
+
+    public function __construct($dns, $user, $pass)
+    {
+        $this->dns = $dns;
+        $this->user = $user;
+        $this->pass = $pass;
+    }
+
+    public function connect()
+    {
+        return new \PDO($this->dns, $this->user, $this->pass);
+    }
+}
---
+++ schoolofnet/php-avancando-com-oo/code_04/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+require_once("IConn.php");
+require_once("Product.php");
+require_once("Conn.php");
+require_once("Container.php");
+
+//$db = new \PDO("mysql:host=localhost;dbname=test_oo","root","123123");
+
+$db = Container::getConn();
+
+$product = Container::getProduct();//new Product($db);
+
+$list = $product->list();
+
+var_dump($list);
