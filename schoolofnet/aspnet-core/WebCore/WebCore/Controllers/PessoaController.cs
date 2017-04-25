3ab82f0461ac288398cea812c36af404dfc1be5a
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-04-25 20:31:26 -0300

    ASP.NET Core MVC - Manipulando Controllers

--- schoolofnet/aspnet-core/WebCore/WebCore/Controllers/PessoaController.cs
+++ schoolofnet/aspnet-core/WebCore/WebCore/Controllers/PessoaController.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebCore.Models.PessoaViewModels;
 
 namespace WebCore.Controllers
 {
     public class PessoaController : Controller
     {
         public IActionResult Index()
-        {
+        {            
             return View();
+        }
+
+        public IActionResult Diferente()
+        {
+            Pessoa pessoa = new Pessoa()
+            {
+                Nome = "Pablo Tôndolo de Vargas",
+                DataNascimento = new DateTime(1986, 12, 12)
+            };
+            return View(pessoa);
+        }
+
+        public IActionResult Teste()
+        {
+            return View("Index");
+        }
+
+        public IActionResult ViewModel()
+        {
+            Pessoa pessoa = new Pessoa()
+            {
+                Nome = "Pablo Tôndolo de Vargas",
+                DataNascimento = new DateTime(1986, 12, 12)
+            };
+            List<Pessoa> lista = new List<Pessoa>();
+            lista.Add(pessoa);
+            return View("Carrega", lista);
         }
     }
 }
