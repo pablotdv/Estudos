1520cd6d611dcab99466fabbf490f7ba6bf2fd2f
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-06-09 15:12:20 -0300

    Backend

---
+++ AdrianHall/Chapter1/Backend/DataObjects/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.DataObjects
+{
+    [Table("TodoItems", Schema = "AdrianHall")]
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
