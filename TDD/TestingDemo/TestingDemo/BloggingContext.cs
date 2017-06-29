6ca8668f533840e0fcdd7f362cf0adbd3dfc4288
Pablo Tôndolo de Vargas <pablotondolo@syns.com.br>
2017-06-29 18:10:21 -0300

    https://msdn.microsoft.com/en-us/library/dn314429(v=vs.113).aspx#Anchor_6

---
+++ TDD/TestingDemo/TestingDemo/BloggingContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingDemo
+{
+    public class BloggingContext : DbContext
+    {
+        public virtual DbSet<Blog> Blogs { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+    }
+}
